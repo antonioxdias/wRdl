8788a5c379dae1c7426f7548ea069c55cc0c3f91
António Dias <a.fortuna.dias@gmail.comm>
2023-07-08 18:15:22 +0100

    Pick word and extract chars. Update flow to use reference to char array instead of reference to char reference array

--- src/main.rs
+++ src/main.rs
@@ -46,26 +46,26 @@ mod row {
     pub struct Row<'a>(Cell<'a>, Cell<'a>, Cell<'a>, Cell<'a>, Cell<'a>);
 
     impl<'a> Row<'a> {
-        pub fn new(response: &[&'a char; 5]) -> Row<'a> {
+        pub fn new(response: &[char; 5]) -> Row {
             Row(
-                Cell::new(response[0]),
-                Cell::new(response[1]),
-                Cell::new(response[2]),
-                Cell::new(response[3]),
-                Cell::new(response[4]),
+                Cell::new(&response[0]),
+                Cell::new(&response[1]),
+                Cell::new(&response[2]),
+                Cell::new(&response[3]),
+                Cell::new(&response[4]),
             )
         }
 
-        pub fn try_to_guess(&mut self, guess: &[&'a char; 5]) {
-            self.0.try_to_guess(guess[0]);
-            self.1.try_to_guess(guess[1]);
-            self.2.try_to_guess(guess[2]);
-            self.3.try_to_guess(guess[3]);
-            self.4.try_to_guess(guess[4]);
+        pub fn try_to_guess(&mut self, guess: &'a [char; 5]) {
+            self.0.try_to_guess(&guess[0]);
+            self.1.try_to_guess(&guess[1]);
+            self.2.try_to_guess(&guess[2]);
+            self.3.try_to_guess(&guess[3]);
+            self.4.try_to_guess(&guess[4]);
         }
     }
 
-    impl<'a> fmt::Display for Row<'a> {
+    impl fmt::Display for Row<'_> {
         fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
             write!(f, "{} {} {} {} {}", self.0, self.1, self.2, self.3, self.4)
         }
@@ -82,7 +82,7 @@ mod board {
     }
 
     impl<'a> Board<'a> {
-        pub fn new(response: &[&'a char; 5]) -> Board<'a> {
+        pub fn new(response: &[char; 5]) -> Board {
             Board {
                 rows: [
                     Row::new(response),
@@ -95,7 +95,7 @@ mod board {
             }
         }
 
-        pub fn try_to_guess(&mut self, guess: &[&'a char; 5]) {
+        pub fn try_to_guess(&mut self, guess: &'a [char; 5]) {
             for (index, row) in self.rows.iter_mut().enumerate() {
                 if index == self.guess_number {
                     row.try_to_guess(guess);
@@ -105,7 +105,7 @@ mod board {
         }
     }
 
-    impl<'a> fmt::Display for Board<'a> {
+    impl fmt::Display for Board<'_> {
         fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
             write!(
                 f,
@@ -118,7 +118,7 @@ mod board {
 
 use board::Board;
 use colored::Colorize;
-use row::Row;
+use rand::Rng;
 
 mod official_allowed_guesses;
 mod shuffled_real_wordles;
@@ -129,28 +129,29 @@ fn greet() {
 
 fn main() {
     greet();
+    println!("\n\n");
 
-    let response0 = 'b';
-    let response1 = 'a';
-    let response2 = 'c';
-    let response3 = 'o';
-    let response4 = 'n';
-    let response = [&response0, &response1, &response2, &response3, &response4];
+    let words = shuffled_real_wordles::SHUFFLED_REAL_WORDLES;
+    let word = words[rand::thread_rng().gen_range(0..=words.len() - 1)];
 
-    let mut row = Row::new(&response);
-    println!("{}", row);
+    println!("The word is {}", word);
+    println!("\n\n");
+
+    let mut response: [char; 5] = ['_'; 5];
+
+    for (index, letter) in word.chars().into_iter().enumerate() {
+        response[index] = letter
+    }
+
+    println!("The response is {:?}", response);
+    println!("\n\n");
 
     let guess0 = 'b';
     let guess1 = 'a';
     let guess2 = 't';
     let guess3 = 'o';
     let guess4 = 'n';
-    let guess = [&guess0, &guess1, &guess2, &guess3, &guess4];
-
-    row.try_to_guess(&guess);
-    println!("{}", row);
-
-    println!("\n\n");
+    let guess = [guess0, guess1, guess2, guess3, guess4];
 
     let mut board = Board::new(&response);
     println!("{}", board);
@@ -162,19 +163,12 @@ fn main() {
 
     println!("\n\n");
 
-    board.try_to_guess(&[&'a', &guess1, &guess2, &guess3, &guess4]);
+    let guess = ['a', guess1, guess2, guess3, guess4];
+    board.try_to_guess(&guess);
     println!("{}", board);
 
     println!("\n\n");
 
     board.try_to_guess(&response);
     println!("{}", board);
-
-    println!("\n\n");
-
-    println!("len {}", shuffled_real_wordles::SHUFFLED_REAL_WORDLES.len());
-    println!(
-        "len {}",
-        official_allowed_guesses::OFFICIAL_ALLOWED_GUESSES.len()
-    );
 }
