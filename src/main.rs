225e0bb00f39f302674361ff380d7188775aca1c
António Dias <a.fortuna.dias@gmail.comm>
2023-07-08 15:15:25 +0100

    Define cell module. Immplement cell.try_to_guess

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,43 @@
-use colored::Colorize;
-use std::fmt;
+mod cell {
+    use colored::Colorize;
+    use std::fmt;
 
-struct Cell<'a> {
-    value: &'a char,
-    guess: Option<&'a char>,
-}
-
-impl<'a> Cell<'a> {
-    fn guess(&self) -> &char {
-        self.guess.unwrap_or(&'_')
+    pub struct Cell<'a> {
+        value: &'a char,
+        guess: Option<&'a char>,
     }
 
-    fn is_correct(&self) -> bool {
-        self.value == self.guess()
-    }
-}
+    impl<'a> Cell<'a> {
+        pub fn new(value: &'a char) -> Cell {
+            Cell { value, guess: None }
+        }
 
-impl<'a> fmt::Display for Cell<'a> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        if self.is_correct() {
-            write!(f, "{}", self.value.to_string().on_green())
-        } else {
-            write!(f, "{}", self.value)
+        fn guess(&self) -> &char {
+            self.guess.unwrap_or(&'_')
+        }
+
+        fn is_correct(&self) -> bool {
+            self.value == self.guess()
+        }
+
+        pub fn try_to_guess(&mut self, guess: &'a char) {
+            self.guess = Some(guess);
+        }
+    }
+
+    impl<'a> fmt::Display for Cell<'a> {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            if self.is_correct() {
+                write!(f, "{}", self.value.to_string().on_green())
+            } else {
+                write!(f, "{}", self.value)
+            }
         }
     }
 }
+
+use cell::Cell;
+use colored::Colorize;
 
 fn greet() {
     println!("\n\nHello, {}_{}ld\n\n", "w".on_yellow(), "r".on_green());
@@ -34,12 +47,16 @@ fn main() {
     greet();
 
     let value = 'a';
-    let guess = Some('a');
 
-    let cell = Cell {
-        value: &value,
-        guess: guess.as_ref(),
-    };
+    let mut cell = Cell::new(&value);
 
+    println!("{}", cell);
+
+    let guess = 'a';
+    cell.try_to_guess(&guess);
+    println!("{}", cell);
+
+    let guess = 'b';
+    cell.try_to_guess(&guess);
     println!("{}", cell);
 }
