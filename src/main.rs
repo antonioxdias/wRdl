4e827d1d26df607964abdf5b0a2239a98df6a8e8
António Dias <a.fortuna.dias@gmail.comm>
2023-07-08 16:33:58 +0100

    Define board module. Refactor Word tuple into fixed size array

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,16 @@ mod cell {
     use std::fmt;
 
     pub struct Cell<'a> {
-        value: &'a char,
+        response: &'a char,
         guess: Option<&'a char>,
     }
 
     impl<'a> Cell<'a> {
-        pub fn new(value: &'a char) -> Cell {
-            Cell { value, guess: None }
+        pub fn new(response: &'a char) -> Cell {
+            Cell {
+                response,
+                guess: None,
+            }
         }
 
         fn guess(&self) -> &char {
@@ -17,7 +20,7 @@ mod cell {
         }
 
         fn is_correct(&self) -> bool {
-            self.value == self.guess()
+            self.response == self.guess()
         }
 
         pub fn try_to_guess(&mut self, guess: &'a char) {
@@ -28,45 +31,37 @@ mod cell {
     impl<'a> fmt::Display for Cell<'a> {
         fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
             if self.is_correct() {
-                write!(f, "{}", self.value.to_string().on_green())
+                write!(f, "{}", self.guess().to_string().on_green())
             } else {
-                write!(f, "{}", self.value)
+                write!(f, "{}", self.guess())
             }
         }
     }
 }
 
 mod row {
-    use crate::Cell;
+    use crate::cell::Cell;
     use std::fmt;
-
-    pub struct Word<'a>(
-        pub &'a char,
-        pub &'a char,
-        pub &'a char,
-        pub &'a char,
-        pub &'a char,
-    );
 
     pub struct Row<'a>(Cell<'a>, Cell<'a>, Cell<'a>, Cell<'a>, Cell<'a>);
 
     impl<'a> Row<'a> {
-        pub fn new(values: Word<'a>) -> Row<'a> {
+        pub fn new(response: &[&'a char; 5]) -> Row<'a> {
             Row(
-                Cell::new(values.0),
-                Cell::new(values.1),
-                Cell::new(values.2),
-                Cell::new(values.3),
-                Cell::new(values.4),
+                Cell::new(response[0]),
+                Cell::new(response[1]),
+                Cell::new(response[2]),
+                Cell::new(response[3]),
+                Cell::new(response[4]),
             )
         }
 
-        pub fn try_to_guess(&mut self, guesses: Word<'a>) {
-            self.0.try_to_guess(guesses.0);
-            self.1.try_to_guess(guesses.1);
-            self.2.try_to_guess(guesses.2);
-            self.3.try_to_guess(guesses.3);
-            self.4.try_to_guess(guesses.4);
+        pub fn try_to_guess(&mut self, guess: &[&'a char; 5]) {
+            self.0.try_to_guess(guess[0]);
+            self.1.try_to_guess(guess[1]);
+            self.2.try_to_guess(guess[2]);
+            self.3.try_to_guess(guess[3]);
+            self.4.try_to_guess(guess[4]);
         }
     }
 
@@ -77,9 +72,53 @@ mod row {
     }
 }
 
-use cell::Cell;
+mod board {
+    use crate::row::Row;
+    use std::fmt;
+
+    pub struct Board<'a> {
+        rows: [Row<'a>; 5],
+        guess_number: usize,
+    }
+
+    impl<'a> Board<'a> {
+        pub fn new(response: &[&'a char; 5]) -> Board<'a> {
+            Board {
+                rows: [
+                    Row::new(response),
+                    Row::new(response),
+                    Row::new(response),
+                    Row::new(response),
+                    Row::new(response),
+                ],
+                guess_number: 0,
+            }
+        }
+
+        pub fn try_to_guess(&mut self, guess: &[&'a char; 5]) {
+            for (index, row) in self.rows.iter_mut().enumerate() {
+                if index == self.guess_number {
+                    row.try_to_guess(guess);
+                };
+            }
+            self.guess_number += 1;
+        }
+    }
+
+    impl<'a> fmt::Display for Board<'a> {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            write!(
+                f,
+                "{}\n{}\n{}\n{}\n{}",
+                self.rows[0], self.rows[1], self.rows[2], self.rows[3], self.rows[4]
+            )
+        }
+    }
+}
+
+use board::Board;
 use colored::Colorize;
-use row::{Row, Word};
+use row::Row;
 
 fn greet() {
     println!("\n\nHello, {}_{}ld\n\n", "w".on_yellow(), "r".on_green());
@@ -88,22 +127,14 @@ fn greet() {
 fn main() {
     greet();
 
-    let value = 'a';
+    let response0 = 'b';
+    let response1 = 'a';
+    let response2 = 'c';
+    let response3 = 'o';
+    let response4 = 'n';
+    let response = [&response0, &response1, &response2, &response3, &response4];
 
-    let mut cell = Cell::new(&value);
-
-    println!("{}", cell);
-
-    let guess = 'a';
-    cell.try_to_guess(&guess);
-    println!("{}", cell);
-
-    let value0 = 'b';
-    let value1 = 'a';
-    let value2 = 'c';
-    let value3 = 'o';
-    let value4 = 'n';
-    let mut row = Row::new(Word(&value0, &value1, &value2, &value3, &value4));
+    let mut row = Row::new(&response);
     println!("{}", row);
 
     let guess0 = 'b';
@@ -111,6 +142,28 @@ fn main() {
     let guess2 = 't';
     let guess3 = 'o';
     let guess4 = 'n';
-    row.try_to_guess(Word(&guess0, &guess1, &guess2, &guess3, &guess4));
+    let guess = [&guess0, &guess1, &guess2, &guess3, &guess4];
+
+    row.try_to_guess(&guess);
     println!("{}", row);
+
+    println!("\n\n");
+
+    let mut board = Board::new(&response);
+    println!("{}", board);
+
+    println!("\n\n");
+
+    board.try_to_guess(&guess);
+    println!("{}", board);
+
+    println!("\n\n");
+
+    board.try_to_guess(&[&'a', &guess1, &guess2, &guess3, &guess4]);
+    println!("{}", board);
+
+    println!("\n\n");
+
+    board.try_to_guess(&response);
+    println!("{}", board);
 }
